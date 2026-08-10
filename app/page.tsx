0e6ae6f8bb8f8f9e08e2dcf8808bd1bed94c39b7
Sebastien Hamon <events.location@outlook.com>
2026-08-10 09:39:52 +0200

    Fix layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import Header from "@/components/Header";
+//import Hero from "@/components/Hero";
+//import Stats from "@/components/Stats";
+//import Services from "@/components/Services";
+//import Gallery from "@/components/Gallery";
+//import WhyChooseUs from "@/components/WhyChooseUs";
+//import EventTypes from "@/components/EventTypes";
+//import Packs from "@/components/Packs";
+//import QuoteForm from "@/components/QuoteForm";
+//import TrustBar from "@/components/TrustBar";
+//import Testimonials from "@/components/Testimonials";
+
+
+
 export default function Home() {
   return (
-    <main style={{ padding: 40 }}>
-      <h1>TEST OK</h1>
-    </main>
+    <>
+      <Header />
+      
+
+
+    </>
   );
 }
